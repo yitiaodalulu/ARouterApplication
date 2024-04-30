6459a539fea0fd789cfb304ea82b26ec4bb4df0f
一条大璐璐 <672277407@qq.com>
2024-04-30 09:41:11 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/arouterapplication/MainActivity.kt
+++ app/src/main/java/com/example/arouterapplication/MainActivity.kt
@@ -7,6 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //a啊实打实大是的撒
     }
 }
